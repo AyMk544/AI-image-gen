04b8b4fac2f3ff9863f4a68bbb82df975f67c41c
AyMk544 <139037561+AyMk544@users.noreply.github.com>
2024-03-10 22:03:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const key2 = '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';
+const key3 = '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';
+const input = document.querySelector('input');
+const btn = document.querySelector('.generate');
+const img = document.querySelector('img');
+
+
+const getImage = async () => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${key3}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                prompt: input.value,
+                n: 1,
+                size: "1024x1024",
+            })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/images/generations', options);
+        const data = await response.json();
+        const url = data.data[0].url;
+        console.log(url);
+        img.setAttribute('src', url);
+        img.classList.remove('none');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+btn.addEventListener('click', getImage);
